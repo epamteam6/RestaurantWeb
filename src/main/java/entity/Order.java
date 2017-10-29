d3b51e3843e9a47736c082a87eb79104a0bc4d35
SergeyAlbrant <albrants95@mail.ru>
2017-10-29 19:00:06 +0300

    changed enum description

--- src/main/java/entity/Order.java
+++ src/main/java/entity/Order.java
@@ -11,5 +11,9 @@ public class Order {
     private int userId;
     private LocalDateTime dateTime;
     private int totalSum;
-    private Enum status;
+    private status st;
+
+    private enum status {
+        CREATED, CONFIRMED, READY, PAID;
+    }
 }
