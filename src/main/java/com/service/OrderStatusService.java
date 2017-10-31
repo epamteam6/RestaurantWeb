0fc2790cf57c9b83c27fc7ec12a7a775db9d53b1
SergeyAlbrant <albrants95@mail.ru>
2017-11-01 00:11:31 +0300

    implemented payOrder and makeBill methods

--- src/main/java/com/service/OrderStatusService.java
+++ src/main/java/com/service/OrderStatusService.java
@@ -8,6 +8,18 @@ public class OrderStatusService {
     public void confirmOrder(int id){
         Order order = OrderDAO.getInstance().getById(id);
         order.setSt(Order.status.CONFIRMED);
-        OrderDAO.getInstance().update(OrderDAO.getInstance().getById(id));
+        OrderDAO.getInstance().update(order);
+    }
+
+    public void payOrder(int id){
+        Order order = OrderDAO.getInstance().getById(id);
+        order.setSt(Order.status.PAID);
+        OrderDAO.getInstance().update(order);
+    }
+
+    public void makeBill(int id){
+        Order order = OrderDAO.getInstance().getById(id);
+        order.setSt(Order.status.READY);
+        OrderDAO.getInstance().update(order);
     }
 }
