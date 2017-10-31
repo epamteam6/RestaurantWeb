d143af8df4d5f338e2043bab56387107efb0061c
Rustam Gurd <rustamkurdov@mail.ru>
2017-10-31 23:52:58 +0300

    Queries created

--- src/main/java/com/dao/DishOrderDAO.java
+++ src/main/java/com/dao/DishOrderDAO.java
@@ -7,7 +7,12 @@ public class DishOrderDAO {
     private static DishOrderDAO instance;
     private DataSource dataSource;
 
-    private static final String GET_BY_ORDER_ID = "";
+    private static final String SELECT_BY_ORDER_ID_QUERY = "SELECT * FROM dishes_orders WHERE order_id = ?";
+    private static final String SELECT_BY_ID_QUERY = "SELECT * FROM dishes_orders WHERE id = ?";
+    private static final String SELECT_ALL_QUERY = "SELECT * FROM dishes_orders";
+    private static final String REMOVE_QUERY = "DELETE * FROM dishes_orders WHERE id = ?";
+    private static final String UPDATE_QUERY = "UPDATE dishes_orders SET order_id = ? dish_id = ? amount = ? sum = ? WHERE id = ?";
+    private static final String INSERT_QUERY = "INSERT INTO dishes_orders VALUES (NULL, ?, ?, ?, ?)";
 
     private DishOrderDAO() {
 
