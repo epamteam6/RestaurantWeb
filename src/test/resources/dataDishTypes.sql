73565dfe38bfbc3366b18fa8b16ac422f4a5c89a
juliaklyueva <juliaklueva@rambler.ru>
2017-10-31 18:18:55 +0300

    table DishTypes for unit tests

---
+++ src/test/resources/dataDishTypes.sql
@@ -0,0 +1,5 @@
+INSERT INTO `DisheTypes` (`id`, `dish_type`) VALUES
+  (1, 'Soups'),
+  (2, 'Desserts'),
+  (3, 'Drinks'),
+  (4, 'Salads');
---
+++ src/test/resources/initDishTypes.sql
@@ -0,0 +1,5 @@
+DROP TABLE IF EXISTS DisheTypes;
+CREATE TABLE DisheTypes (
+  id        BIGINT AUTO_INCREMENT PRIMARY KEY,
+  dish_type VARCHAR(32) NOT NULL,
+);
