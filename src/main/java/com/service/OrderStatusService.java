248b21a86c40a42fae803390d5cccba7b8192d1c
SergeyAlbrant <albrants95@mail.ru>
2017-11-01 16:36:08 +0300

    refactored and made it singletone

--- src/main/java/com/service/OrderStatusService.java
+++ src/main/java/com/service/OrderStatusService.java
@@ -3,23 +3,37 @@ package com.service;
 import com.dao.OrderDAO;
 import com.model.Order;
 
+import javax.sql.DataSource;
+
 public class OrderStatusService {
 
+    private OrderDAO orderDAO = OrderDAO.getInstance();
+    private static OrderStatusService instance;
+
+    private OrderStatusService() { }
+
+    public static OrderStatusService getInstance() {
+        if (instance == null) {
+            instance = new OrderStatusService();
+        }
+        return instance;
+    }
+
     public void confirmOrder(int id){
-        Order order = OrderDAO.getInstance().getById(id);
-        order.setSt(Order.status.CONFIRMED);
-        OrderDAO.getInstance().update(order);
+        Order order = orderDAO.getById(id).get();
+        order.setStatus(Order.Status.CONFIRMED);
+        orderDAO.update(order);
     }
 
     public void payOrder(int id){
-        Order order = OrderDAO.getInstance().getById(id);
-        order.setSt(Order.status.PAID);
-        OrderDAO.getInstance().update(order);
+        Order order = orderDAO.getById(id).get();
+        order.setStatus(Order.Status.PAID);
+        orderDAO.update(order);
     }
 
     public void makeBill(int id){
-        Order order = OrderDAO.getInstance().getById(id);
-        order.setSt(Order.status.READY);
-        OrderDAO.getInstance().update(order);
+        Order order = orderDAO.getById(id).get();
+        order.setStatus(Order.Status.READY);
+        orderDAO.update(order);
     }
 }
