da8c54e1ec1ce6ca61f5e383471279e6a620feed
Rustam Gurd <rustamkurdov@mail.ru>
2017-11-02 03:41:29 +0300

    AuthorisationService implementation

--- src/main/java/com/service/AuthorisationService.java
+++ src/main/java/com/service/AuthorisationService.java
@@ -11,6 +11,7 @@ public class AuthorisationService {
 
     private DataSource dataSource;
     private UserDAO userDAO = UserDAO.getInstance();
+
     {
         userDAO.setDataSource(dataSource);
     }
@@ -23,14 +24,24 @@ public class AuthorisationService {
     }
 
     public AuthorisationService getInstance() {
+        if (instance == null)
+            instance = new AuthorisationService();
+
         return instance;
     }
 
-    public void singIn(String userName, String password) {
+    public boolean singIn(String userName, String password) {
         if (userDAO.validate(userName, password)) {
             user = userDAO.getByName(userName).get();
+
+            return true;
         }
+
+        return false;
     }
 
-
+    public void singOut() {
+        // TODO: 02.11.2017
+        user = null;
+    }
 }
