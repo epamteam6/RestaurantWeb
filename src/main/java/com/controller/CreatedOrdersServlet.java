7ba7dfa942f729fd39fba63ec1e618586a2ec553
Rustam Gurd <rustamkurdov@mail.ru>
2017-11-09 22:26:34 +0300

    User page with new orders

---
+++ src/main/java/com/controller/CreatedOrdersServlet.java
@@ -0,0 +1,69 @@
+package com.controller;
+
+import com.model.Order;
+import com.model.User;
+import com.service.OrderService;
+import com.service.OrderStatusService;
+import com.service.UserService;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.*;
+
+public class CreatedOrdersServlet extends HttpServlet {
+
+    private Map<String, Map<String, Long>> menu;
+    private UserService userService = UserService.getInstance();
+    private OrderService orderService = OrderService.getInstance();
+    private OrderStatusService orderStatusService = OrderStatusService.getInstance();
+    private Map<String, Map<Long, Map<String, Long>>> usersOrders;
+    private List<Long> orderNumbers;
+
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        List<User> allUsers = userService.getUserDAO().getAll();
+        System.out.println(allUsers);
+
+        usersOrders = new HashMap<>();
+        orderNumbers = new ArrayList<>();
+
+        String username = (String) request.getSession().getAttribute("loggedInUser");
+        Optional<User> optional = userService.getUserByName(username);
+
+        if (!optional.isPresent())
+        {
+            response.sendRedirect("make_order.jsp");
+            return;
+        }
+
+        User user = optional.get();
+        Map<Long, Map<String, Long>> ordersDetails = orderService.orderDetails(user.getUserName(), Order.Status.CREATED);
+        if (!ordersDetails.isEmpty()) {
+            for (Long number : ordersDetails.keySet()) {
+                orderNumbers.add(number);
+            }
+            usersOrders.put(user.getUserName(), ordersDetails);
+        }
+
+        request.setAttribute("usersOrders", usersOrders);
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/created_orders.jsp");
+        if (dispatcher != null) {
+            dispatcher.forward(request, response);
+        }
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        request.getRequestDispatcher("/created_orders.jsp").include(request, response);
+    }
+}
