33c1ff42456f83cef15882e1dca935745be9f6eb
SergeyAlbrant <albrants95@mail.ru>
2017-11-09 15:16:05 +0300

    created 2 new servlets for admin functions

---
+++ src/main/java/com/controller/UnPaidOrdersServlet.java
@@ -0,0 +1,74 @@
+package com.controller;
+
+import com.model.Order;
+import com.model.User;
+import com.service.OrderService;
+import com.service.UserService;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class UnPaidOrdersServlet extends HttpServlet {
+
+    private UserService userService = UserService.getInstance();
+    private OrderService orderService = OrderService.getInstance();
+    private Map<String, Map<Long, Map<String, Long>>> usersOrders;
+    private List<Long> orderNumbers;
+
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        List<User> allUsers = userService.getUserDAO().getAll();
+        System.out.println(allUsers);
+
+        usersOrders = new HashMap<>();
+        orderNumbers = new ArrayList<>();
+
+        for (User user : allUsers) {
+            Map<Long, Map<String, Long>> ordersDetails = orderService.orderDetails(user.getUserName(), Order.Status.READY);
+            if (!ordersDetails.isEmpty()) {
+                for (Long number : ordersDetails.keySet()) {
+                    orderNumbers.add(number);
+                }
+                usersOrders.put(user.getUserName(), ordersDetails);
+            }
+        }
+
+        request.setAttribute("usersOrders", usersOrders);
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/completed_orders.jsp");
+        if (dispatcher != null) {
+            dispatcher.forward(request, response);
+        }
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("completed_orders.jsp").include(request, response);
+
+
+        System.out.println(orderNumbers);
+
+        for (Long number : orderNumbers) {
+            Boolean checked = request.getParameter(number.toString()) != null;
+            if (checked) {
+                orderService.cancelOrder(number);
+            }
+        }
+
+
+        response.sendRedirect("/success.jsp");
+
+    }
+}
