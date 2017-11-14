c970da77569685cff33947a78143e8525471d070
Rustam Gurd <rustamkurdov@mail.ru>
2017-11-14 23:23:32 +0300

    Logging implementation:
    AboutServlet

--- src/main/java/com/controller/user/AboutServlet.java
+++ src/main/java/com/controller/user/AboutServlet.java
@@ -1,5 +1,7 @@
 package com.controller.user;
 
+import org.apache.log4j.Logger;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -9,9 +11,13 @@ import java.io.IOException;
 
 public class AboutServlet extends HttpServlet {
 
+    private static final Logger log = Logger.getLogger(AboutServlet.class);
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+
+        log.info("(session) Initializing...");
 
         request.setAttribute("username", (String) request.getSession().getAttribute("loggedInUser"));
 
@@ -24,6 +30,8 @@ public class AboutServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        log.info("(session) Processing...");
 
         request.setAttribute("username", (String) request.getSession().getAttribute("loggedInUser"));
         request.getRequestDispatcher("about.jsp").include(request, response);
