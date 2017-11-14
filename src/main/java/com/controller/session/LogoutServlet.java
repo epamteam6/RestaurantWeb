2fc867e310f4748452f80eca42e8be26fee73ed7
Rustam Gurd <rustamkurdov@mail.ru>
2017-11-14 23:20:28 +0300

    Logging implementation:
    LogoutServlet

--- src/main/java/com/controller/session/LogoutServlet.java
+++ src/main/java/com/controller/session/LogoutServlet.java
@@ -1,5 +1,7 @@
 package com.controller.session;
 
+import org.apache.log4j.Logger;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -9,9 +11,13 @@ import java.io.IOException;
 
 public class LogoutServlet extends HttpServlet {
 
+    private static final Logger log = Logger.getLogger(LogoutServlet.class);
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+
+        log.info("(session) Initializing...");
 
         //System.out.println(request.getSession().getAttribute("loggedInUser"));
         request.getSession().setAttribute("loggedInUser", null);
